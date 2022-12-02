2ff0ae32b654693080f31e6f8a63c5782f81f4f1
Maksemqwe123 <101874489+Maksemqwe123@users.noreply.github.com>
2022-12-02 19:53:23 +0300

    add def and deleting comments

--- New_life_3/parser_pizza.py
+++ New_life_3/parser_pizza.py
@@ -1,17 +1,20 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = f'https://ym1.by/akczii/'
-
-response = requests.get(url=url)
-
-pages_info = BeautifulSoup(response.text, 'html.parser')
-
-ref = pages_info.find('article', class_='akcii-i post-1011 akczii type-akczii status-publish has-post-thumbnail hentry').find('a').get('href')
+list_pizza = []
 
 
+def deals():
+    url = f'https://ym1.by/akczii/'
 
-# car_names = pages_info.find_all('a', class_='listing-item__link')
-#         for name in car_names:
-#             self.items.append(name.text)
-#             self.urls.append(f"https://cars.av.by/bmw{name['href']}")
+    response = requests.get(url=url)
+
+    pages_info = BeautifulSoup(response.text, 'html.parser')
+
+    pizza = pages_info.find('article', class_='akcii-i post-1011 akczii type-akczii status-publish has-post-thumbnail hentry').find('a').get('href')
+    list_pizza.append(pizza)
+
+    return list_pizza
+
+
+parser_pizza = deals()
