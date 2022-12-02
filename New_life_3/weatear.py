87d193bb141e624a2ee39b5c2b88fc5d38188249
Maksemqwe123 <101874489+Maksemqwe123@users.noreply.github.com>
2022-12-02 19:56:04 +0300

    add capslock

--- New_life_3/weatear.py
+++ New_life_3/weatear.py
@@ -1,17 +1,15 @@
 import requests
-from pprint import pprint
-from config import open_weather_token
+from config import OPEN_WEATHER_TOKEN
 
 ru = 'ru'
 
 
-def get_weather(city, open_weather_token):
+def get_weather(city, OPEN_WEATHER_TOKEN):
     try:
         r = requests.get(
-            f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={open_weather_token}&units=metric&lang={ru}"
+            f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={OPEN_WEATHER_TOKEN}&units=metric&lang={ru}"
         )
         data = r.json()
-        # pprint(data)
 
         city = data["name"]
         cur_weather = data["main"]["temp"]
@@ -28,8 +26,9 @@ def get_weather(city, open_weather_token):
 
 def main():
     city = input('Введите город')
-    get_weather(city, open_weather_token)
+    get_weather(city, OPEN_WEATHER_TOKEN)
 
 
 if __name__ == '__main__':
     main()
+    
